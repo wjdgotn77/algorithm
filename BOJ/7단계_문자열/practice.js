20b5f8ea15ca6d22ad87b1804cd4bc817fdbacc6
haesoo <wjdgotn77@gmail.com>
2021-10-22 23:29:22 +0900

    add: 백준 7단계 1152번 풀이

--- BOJ/7단계_문자열/practice.js
+++ BOJ/7단계_문자열/practice.js
@@ -1,19 +1,8 @@
-const input = ["2", "3 ABC", "5 /HTP"];
-const idx = +input[0];
-let result = "";
+const input = "The Curious Case of Benjamin Button".trim().split(" ");
 
-for (let i = 1; i <= idx; i++) {
-  const test = input[i].split(" ");
-
-  const count = Number(test[0]);
-  const words = test[1].split("");
-
-  for (let j = 0; j < words.length; j++) {
-    for (let k = 0; k < count; k++) {
-      result += words[j];
-    }
-  }
-  result += "\n";
+if (input[0] === "") {
+    console.log(0);
+} else {
+    console.log(input.length);
 }
-
-console.log(result);
+console.log(input.length);
